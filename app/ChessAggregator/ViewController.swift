c5930fed4caadb694a03a427acb76f731e8da7c0
NickPuchko <47064996+NickPuchko@users.noreply.github.com>
2020-10-30 17:44:25 +0300

    init

---
+++ app/ChessAggregator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChessAggregator
+//
+//  Created by Николай Пучко on 21.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
