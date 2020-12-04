c4e9e6a7d53d549cf3995f897a00a36d914ed82f
NickPuchko <47064996+NickPuchko@users.noreply.github.com>
2020-12-04 15:25:33 +0300

    player entity

--- app/ChessAggregator/Entities/Player.swift
+++ app/ChessAggregator/Entities/Player.swift
@@ -3,12 +3,12 @@ import Foundation
 struct Player: Codable {
     var fullName: String
     var birthdate: Date
-    var rating: Int // TODO: replace with parsed id's only
-    
+    var rating: Int // TODO: replace with getter property - parsed by id
+    var sex: String // TODO: replace with enum
     var fideID: Int?
     var frcID: Int?
 
-    init(fullName: String = "John Doe", birthdate: Date = Calendar.current.date(from: DateComponents(year: 1953, month: 2, day: 25))!, rating: Int?, fideID: Int? = 24176214, frcID: Int? = 1606) {
+    init(fullName: String = "John Doe", birthdate: Date = Calendar.current.date(from: DateComponents(year: 1953, month: 2, day: 25))!, rating: Int?, sex: String = "Male", fideID: Int? = 24176214, frcID: Int? = 1606) {
         self.fullName = fullName
         self.birthdate = birthdate
         if let ELOrating = rating {
@@ -16,6 +16,7 @@ struct Player: Codable {
         } else {
             self.rating = 0
         }
+        self.sex = sex
         self.fideID = fideID
         self.frcID = frcID
     }
