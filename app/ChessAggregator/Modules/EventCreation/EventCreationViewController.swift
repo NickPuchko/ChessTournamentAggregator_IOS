f8e41d28f366eb90b4697eebd16565ac94091610
NickPuchko <47064996+NickPuchko@users.noreply.github.com>
2021-01-28 03:00:07 +0300

    Responder adapting scroll in EventCreation

--- app/ChessAggregator/Modules/EventCreation/EventCreationViewController.swift
+++ app/ChessAggregator/Modules/EventCreation/EventCreationViewController.swift
@@ -103,10 +103,30 @@ final class EventCreationViewController: UIViewController, UIScrollViewDelegate 
                 UIBarButtonItem(image: UIImage(systemName: "xmark"), style: .plain, target: self, action: #selector(endCreation))
         navigationItem.rightBarButtonItem =
                 UIBarButtonItem(title: "Опубликовать", style: .done, target: self, action: #selector(createDefault))
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
 	}
 
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
          view.endEditing(true)
+    }
+
+    @objc func keyboardWillShow(notification:NSNotification) {
+
+        guard let userInfo = notification.userInfo else { return }
+        var keyboardFrame:CGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+
+        var contentInset:UIEdgeInsets = scrollableStackView.scrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height + 20
+        scrollableStackView.scrollView.contentInset = contentInset
+    }
+
+    @objc func keyboardWillHide(notification:NSNotification) {
+
+        let contentInset:UIEdgeInsets = UIEdgeInsets.zero
+        scrollableStackView.scrollView.contentInset = contentInset
     }
 
     @objc
